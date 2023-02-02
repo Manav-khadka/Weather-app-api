747321259e2047676d7fff8050fc401e665abe47
Manav Khadka <96826294+Manav-khadka@users.noreply.github.com>
2023-02-02 06:42:57 +0000

    mentor

---
+++ script.js
@@ -0,0 +1,52 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "dd18910a12msh4a1346014f9c7dcp1b94d3jsn0d3e38c9f29e",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+
+getWeather = (city) => {
+	cityName.innerHTML = city;
+
+  fetch(
+    `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      // cloud_pct.innerHTML = response.cloud_pct;
+      temp.innerHTML = response.temp;
+      temp2.innerHTML = response.temp;
+      feels_like.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      humidity2.innerHTML = response.humidity;
+      min_temp.innerHTML = response.min_temp;
+      max_temp.innerHTML = response.max_temp;
+      wind_speed.innerHTML = response.wind_speed;
+      wind_speed2.innerHTML = response.wind_speed;
+      wind_degrees.innerHTML = response.wind_degrees;
+      sunrise.innerHTML = response.sunrise;
+      sunset.innerHTML = response.sunset;
+    })
+    .catch((err) => console.error(err));
+	
+};
+// let searchInputBox = document.getElementById('cityName');
+// searchInputBox.addEventListener(`keypress`, (event) => {
+// 	// e.preventDefault();
+    
+// 	if (event.keyCode == 13) {
+//         // console.log(searchInputBox.value);
+//         getWeatherReport(searchInputBox.value);
+        
+//     }
+        
+    
+// })
+submit.addEventListener("click", (e) => {
+	e.preventDefault();
+	getWeather(city.value);
+  });
+getWeather("delhi");
